a83c3dcdc92b6d945cfc4ed0795791e1566dc071
deltamaze <wcpooley@gmail.com>
2018-02-06 21:58:23 -0600

    pass data from client to server

--- online-multiplayer/server/app.js
+++ online-multiplayer/server/app.js
@@ -27,114 +27,80 @@ http.listen(PORT, function () {
 
 const io = require('socket.io')(http);
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-
-    socket.on('chat message', function(msg){
-          console.log('message: ' + msg);
-          io.emit('chat message', test);
-        });
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
-    });
-
-  });
-//socket methods
-//relay a gameItemObject, with player pos, ball pos/velocity, round num, scores
-//on client if roundnum != the server roundnum, then update round num, and ball pos, otherwise, update ball pos if ball is not on their side of the court
-
-
-//RESTful methods
-
-
-function GameService() {
-
-    this.gameUpdateTime = 100;
-    this.canvasHeight = 400;
-    this.canvasWidth = 800;
-    this.room = 'Main';
+io.on('connection', function (socket) {
 
     this.frameCounter = 0;
     this.myTimer;
     this.gameEngine;
 
+    console.log('a user connected');
+    //joinGame
+    socket.on('joinGame', function (joinGameInfo) {
+        console.log(joinGameInfo);
+        addPlayerToGame();
+        // if (gameObjects[roomName] == [] || gameObjects[roomName] == null) {
 
-    this.playerJoinGame = function (roomName) {
-        //check to see if game exist
-        console.log(gameObjects);
-        if(gameObjects[roomName] == [] || gameObjects[roomName] == null)
-        {
-            
-            console.log("test");
-            gameObjects[roomName] =  {
-                players:[],
-                inProgress:false
-            }
-            this.addPlayerToGame();
-        }
-        else if(gameObjects[roomName].players.Length <= 2)
-        {
-            this.addPlayerToGame();
-        }
-        
-        this.room = roomName
-        console.log("start game called:"+ roomName.toString());
-        //this.myTimer = setInterval(this.gameEngine.bind(this), this.gameUpdateTime);
-    }
+        //     console.log("test");
+        //     gameObjects[roomName] = {
+        //         players: [],
+        //         inProgress: false
+        //     }
+        //     this.addPlayerToGame();
+        // }
+        // else if (gameObjects[roomName].players.Length <= 2) {
+        //     this.addPlayerToGame();
+        // }
 
-
-
-
-
-
-
-
-
+        // this.room = roomName
+        // console.log("start game called:" + roomName.toString());
+        // //this.myTimer = setInterval(this.gameEngine.bind(this), this.gameUpdateTime);
+        // io.emit('chat message', test);
+    });
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', test);
+    });
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 
 
-
-    this.addPlayerToGame = function ()
-    {
+    addPlayerToGame = function () {
         console.log("test");
 
     }
 
-    this.gameEngine = function () {
+    gameEngine = function () {
         this.frameCounter++;
-        
-  
         this.checkGameOver();//check to see if players lost
         //also end game if gametime exceeds 10 minutes incase player afk
-       
+
         console.log();
         if (this.frameCounter > 3000)//if both players dead, or game time past 10 minutes, end timer
         {
             clearInterval(this.myTimer);
-            
         }
     }
-
-
-    this.checkGameOver = function () {
+    hash = function(guid) {
+        var hash = 0, i, chr;
+        if (guid.length === 0) return hash;
+        for (i = 0; i < guid.length; i++) {
+            chr = guid.charCodeAt(i);
+            hash = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return hash;
+    }
+    checkGameOver = function () {
         //if score hits score limit, end game, return true
-
     }
-    this.updateSnake = function () {
-
-    }
-    this.resetPosition = function (playerNum) {
-
+    resetPosition = function (playerNum) {
         //move players back to spawn, and tell clients ball angular velocity so that it's synced up on both clients
     }
-}
-
-
-
-
-//todo generate token method
-// create mutex for gameroom + player token
-
-
+});
+//socket methods
+//relay a gameItemObject, with player pos, ball pos/velocity, round num, scores
+//on client if roundnum != the server roundnum, then update round num, and ball pos, otherwise, update ball pos if ball is not on their side of the court
 
 
 
